5ea4bc8bcd23b8e51fa75ea51b2a1fe283b6bd16
Harshit Shukla <harshitcr7777@gmail.com>
2024-08-01 22:08:53 +0530

    made section2 responsive

--- src/components/section-2.jsx
+++ src/components/section-2.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Section2 = () => {
   return (
-    <div className="px-20 text-white">
+    <div className="px-4 md:px-20 text-white">
       <div className="border-2 border-borderColor">
-        <div className="bg-primaryGradient">
-          <span className="font-[600] text-[36px] md:text-[86px] text-white w-full flex justify-center py-20">
+        <div className="bg-primaryGradient py-6">
+          <span className="font-[600] text-[36px] md:text-[86px] text-white w-full flex justify-center py-6 md:py-14">
             About BotTradeX
           </span>
-          <p className="text-white text-[18px] md:text-[30px] leading-[1.2] text-center px-12 pb-20">
+          <p className="text-light-gray text-[18px] md:text-[30px] md:leading-[1.2] text-center px-4 md:px-12 pb-6 md:pb-14">
             BotTradeX is a leading Queensland-based provider of cutting-edge
             trading automation solutions. Specializing in high-performance
             Expert Advisors (EAs), advanced High-Frequency Trading (HFT)
@@ -23,14 +23,20 @@ const Section2 = () => {
 
         <div className="w-full flex items-center flex-col py-20">
           <div className="font-[600] text-[36px] md:text-[86px] text-white flex flex-col w-full items-center pb-16">
-            <span className="-mb-10">Why Choose</span>
+            <span className="md:-mb-10">Why Choose</span>
             <span className=""> BotTradeX?</span>
           </div>
-          <div className="grid grid-cols-2 px-8 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 px-4 md:px-8 gap-4 md:gap-8">
             <div className="bg-card-background flex flex-col p-8 rounded-2xl gap-8 justify-between">
-              <img src="/images/ai.svg" alt="ai" className="w-[200px]" />
-              <span className="text-[40px]">AI-Powered Analysis</span>
-              <p className="text-[32px] text-light-gray leading-[1.2]">
+              <img
+                src="/images/ai.svg"
+                alt="ai"
+                className="w-1/2 md:w-[200px]"
+              />
+              <span className="text-[36px] leading-[1.2] md:text-[40px]">
+                AI-Powered Analysis
+              </span>
+              <p className="text-[18px] md:text-[32px] text-light-gray leading-[1.2]">
                 Proven rule-based algorithms, Our rule-based algorithms are
                 rigorously backtested to ensure reliable performance over time.
               </p>
@@ -39,9 +45,15 @@ const Section2 = () => {
               </button>
             </div>
             <div className="bg-card-background flex flex-col p-8 rounded-2xl gap-8 justify-between">
-              <img src="/images/alarm.svg" alt="ai" className="w-[200px]" />
-              <span className="text-[40px]">Real-Time Alerts</span>
-              <p className="text-[32px] text-light-gray leading-[1.2]">
+              <img
+                src="/images/alarm.svg"
+                alt="ai"
+                className="w-1/2 md:w-[200px]"
+              />
+              <span className="text-[36px] leading-[1.2] md:text-[40px]">
+                Real-Time Alerts
+              </span>
+              <p className="text-[18px] md:text-[32px] text-light-gray leading-[1.2]">
                 Stay ahead of the market with real-time alerts and notifications
                 tailored to your trading strategy.
               </p>
