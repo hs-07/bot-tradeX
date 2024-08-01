e7548ceee6048bc8cb1cc61d991ea266599dc54f
Harshit Shukla <harshitcr7777@gmail.com>
2024-08-01 22:37:26 +0530

    made section4 & 5 responsive

--- src/components/section-4.jsx
+++ src/components/section-4.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const section4 = () => {
   return (
-    <div className="px-20 ">
-      <div className="flex w-full border-x-2 border-borderColor py-20 px-6 gap-6 items-center">
-        <div className="w-1/2">
-          <span className="font-[600] text-[36px] md:text-[86px] text-white flex flex-col w-full py-6">
+    <div className="px-4 md:px-20 ">
+      <div className="flex md:flex-row flex-col w-full border-x-2 border-borderColor py-20 px-6 gap-6 items-center">
+        <div className="w-full md:w-1/2 md:order-1 order-2">
+          <span className="font-[600] text-[36px] md:text-[86px] text-white flex flex-col w-full py-4 md:py-6">
             Our Approach
           </span>
 
-          <p className="text-[30px] text-light-gray leading-[1.2] text-start">
+          <p className="text-[18px] md:text-[30px] text-light-gray md:leading-[1.2] text-start">
             We are committed to redefining algorithmic trading by combining
             state-of-the-art technology with extensive market knowledge. Our
             vision is to set new standards in the industry and create a dynamic
@@ -17,7 +17,7 @@ const section4 = () => {
             clients.
           </p>
         </div>
-        <div className="w-1/2 h-full">
+        <div className="w-full md:w-1/2 h-full md:order-2 order-1">
           <img
             src="/images/hero-4.svg"
             alt="approach"
