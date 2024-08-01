a140200b66c7dc20b4423dec480bb20e35b77e62
Harshit Shukla <harshitcr7777@gmail.com>
2024-08-01 19:50:00 +0530

    commiting section7

---
+++ src/utils/constants.js
@@ -0,0 +1,64 @@
+export const packages = [
+  {
+    plan: "Free Plan",
+    cost: "AUD $0",
+    subtext: "Beginners interested in automated trading",
+    features: [
+      "Basic BotTradeX EA License (limited functionality)",
+      "Access to the BotTradeX community forum",
+      "Regular updates and news on trading strategies",
+    ],
+    benefits: [
+      "Get a feel for automated trading without any financial commitment",
+      "Community Support: Engage with other traders in the BotTradeX community to share insights and gain knowledge.",
+      "Stay Informed: Receive updates on trading strategies and market trends to enhance your trading knowledge.",
+    ],
+  },
+  {
+    plan: "Starter Package",
+    cost: "AUD $9,990",
+    subtext: "Novice traders",
+    features: [
+      "Lifetime BotTradeX EA License Pre-configured set files using BotTradeX's proven default strategy",
+    ],
+    benefits: [
+      "Effortlessly integrate automated trading into your existing platform",
+      "Gain hands-on experience with a reliable, pre-optimized strategy",
+      "Establish a strong foundation for further exploration and customization",
+    ],
+  },
+  {
+    plan: "Premium Package",
+    cost: "AUD $14,990",
+    subtext: " Traders seeking to expand their strategies",
+    features: [
+      "Lifetime BotTradeX EA License 2x set files",
+      "Default BotTradeX strategy",
+      "3x confluence strategy for diversified market exposure",
+      "3 months of priority customer support",
+    ],
+    benefits: [
+      "All Starter Package benefits plus",
+      "Explore diverse trading approaches with multiple pre-configured strategies",
+      "Accelerate your learning curve and achieve consistent results",
+    ],
+  },
+  {
+    plan: "Elite Package",
+    cost: "AUD $19,990",
+    subtext: "Seasoned traders demanding peak performance",
+    features: [
+      "Lifetime BotTradeX EA License 3x set files",
+      "Default BotTradeX strategy",
+      "Custom strategy after a comprehensive optimization session",
+      "6 months of priority customer support",
+      "1x in-depth strategy optimization consultation with a BotTradeX expert",
+    ],
+    benefits: [
+      "All Premium Package benefits plus:",
+      "Harness the full power of BotTradeX with a personalized, optimized strategy",
+      "Gain unparalleled insights and fine-tune your approach with expert guidance",
+      "Maximize your profitability and unlock your full potential in the markets",
+    ],
+  },
+];
