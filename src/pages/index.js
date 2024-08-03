5e4c99cd4a3e369cf7f64268d742ab164e933591
Harshit Shukla <harshitcr7777@gmail.com>
2024-08-03 14:34:33 +0530

    commiting completed the website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
 export { default as LandingPage } from "./landing";
+export { default as About } from "./about";
+export { default as TermsAndConditions } from "./TermsConditions";
+export { default as PrivacyPolicy } from "./PrivacyPolicy";
