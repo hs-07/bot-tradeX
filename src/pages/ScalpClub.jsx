b27abed6ca9709a56c274a98f1e52342e7df292b
Harshit Shukla <harshitcr7777@gmail.com>
2024-08-15 17:46:33 +0530

    commiting sectio4

--- src/pages/ScalpClub.jsx
+++ src/pages/ScalpClub.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Section1, Section2, Section3 } from "../components/ScalpClub";
+import {
+  Section1,
+  Section2,
+  Section3,
+  Section4,
+  Section5,
+} from "../components/ScalpClub";
 import { Navbar, Underline } from "../components";
 
 const ScalpClub = () => {
@@ -10,6 +16,10 @@ const ScalpClub = () => {
       <Section2 id="section2" />
       <Underline />
       <Section3 id="section3" />
+      <Underline />
+      <Section4 id="section4" />
+      <Underline />
+      <Section5 id="section5" />
     </div>
   );
 };
